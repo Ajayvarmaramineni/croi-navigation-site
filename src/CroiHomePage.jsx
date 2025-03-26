273959c7f31f3e2d7aee70ab1b85507c7d2f2e3d
ajayvarmaramineni <aramineni@wpi.edu>
2025-03-26 00:34:10 -0400

    Pushing clean project with build and source files only

---
+++ src/CroiHomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CroiHomePage() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1 style={{ color: "#27ae60" }}>Welcome to Croi.io</h1>
+      <p style={{ color: "#2c3e50" }}>Empowering Smarter College Choices</p>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CroiHomePage from "./CroiHomePage";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CroiHomePage />
+  </React.StrictMode>
+);
